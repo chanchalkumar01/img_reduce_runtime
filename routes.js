1e4e0f164cec2dddf174cebf939c8dde5f759ab1
chanchalkumar01 <141403710+chanchalkumar01@users.noreply.github.com>
2025-02-06 18:51:05 +0530

    Update routes.js
    update route file for qulaity

--- routes.js
+++ routes.js
@@ -12,22 +12,36 @@ router.get('/example', (req, res) => {
 });
 
 
-router.get('/image_kit/:width_height/:image_name', async (req, res) => {
+// router.get('/image_kit/:width_height/:image_name', async (req, res) => {
    
-    const { width_height, image_name } = req.params;
+//     const { width_height, image_name } = req.params;
+
+//     try {
+//         const imageProcessor = new ImageProcessor();
+//         await imageProcessor.processImage(width_height, image_name, res);
+
+//         // res.set('Link', '<link rel="icon" href="https://ik.imagekit.io/pu0hxo64d/images/favicon.ico" type="image/x-icon">');
+//         // res.setHeader('Title', image_name); 
+
+//     } catch (error) {
+//         console.error('Error processing image:', error);
+//         res.status(500).send('Internal Server Error');
+//     }
+// });
+
+
+router.get('/image_kit/:width_height/:image_name/:quality?', async (req, res) => {
+    const { width_height, image_name, quality } = req.params;
 
     try {
         const imageProcessor = new ImageProcessor();
-        await imageProcessor.processImage(width_height, image_name, res);
-
-        // res.set('Link', '<link rel="icon" href="https://ik.imagekit.io/pu0hxo64d/images/favicon.ico" type="image/x-icon">');
-        // res.setHeader('Title', image_name); 
-
+        await imageProcessor.processImage(`${width_height},q-${quality || 100}`, image_name, res);
     } catch (error) {
         console.error('Error processing image:', error);
         res.status(500).send('Internal Server Error');
     }
 });
+
 
 // Export the router
 module.exports = router;
